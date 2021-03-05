6f8ad32bcd3c292c934b9f3f8530da0449698408
John Tucker <jtucker@spotify.com>
2021-03-04 22:00:19 -0500

    example

--- internal/workload/workload.go
+++ internal/workload/workload.go
@@ -1,7 +1,35 @@
 package workload
 
-import "fmt"
+import (
+	"context"
+	"log"
+	"net"
+
+	pb "github.com/larkintuckerllc/workload/pkg/workload"
+	"google.golang.org/grpc"
+)
+
+const (
+	port = ":50051"
+)
+
+type server struct {
+	pb.UnimplementedGreeterServer
+}
+
+func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
 
 func Execute() {
-	fmt.Println("Hello, World!")
+	lis, err := net.Listen("tcp", port)
+	if err != nil {
+		log.Fatalf("failed to listen: %v", err)
+	}
+	s := grpc.NewServer()
+	pb.RegisterGreeterServer(s, &server{})
+	if err := s.Serve(lis); err != nil {
+		log.Fatalf("failed to serve: %v", err)
+	}
 }
