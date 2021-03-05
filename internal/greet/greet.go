0da14755dee3eb69e3a8e418cdb018d73de1fc14
John Tucker <jtucker@spotify.com>
2021-03-05 06:22:01 -0700

    refactor

---
+++ internal/greet/greet.go
@@ -0,0 +1,17 @@
+package greet
+
+import (
+	"context"
+	"log"
+
+	pb "github.com/larkintuckerllc/workload/pkg/greet"
+)
+
+type GreeterServer struct {
+	pb.UnimplementedGreeterServer
+}
+
+func (s *GreeterServer) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
+	log.Printf("Received: %v", in.GetName())
+	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
+}
--- internal/workload/workload.go
+++ internal/workload/workload.go
@@ -1,11 +1,11 @@
 package workload
 
 import (
-	"context"
 	"log"
 	"net"
 
-	pb "github.com/larkintuckerllc/workload/pkg/greet"
+	"github.com/larkintuckerllc/workload/internal/greet"
+	greetPb "github.com/larkintuckerllc/workload/pkg/greet"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/reflection"
 )
@@ -14,22 +14,13 @@ const (
 	port = ":50051"
 )
 
-type server struct {
-	pb.UnimplementedGreeterServer
-}
-
-func (s *server) SayHello(ctx context.Context, in *pb.HelloRequest) (*pb.HelloReply, error) {
-	log.Printf("Received: %v", in.GetName())
-	return &pb.HelloReply{Message: "Hello " + in.GetName()}, nil
-}
-
 func Execute() {
 	lis, err := net.Listen("tcp", port)
 	if err != nil {
 		log.Fatalf("failed to listen: %v", err)
 	}
 	s := grpc.NewServer()
-	pb.RegisterGreeterServer(s, &server{})
+	greetPb.RegisterGreeterServer(s, &greet.GreeterServer{})
 	reflection.Register(s)
 	if err := s.Serve(lis); err != nil {
 		log.Fatalf("failed to serve: %v", err)
